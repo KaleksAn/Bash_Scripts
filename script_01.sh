b1d4989fd49a8c4ee438ca877776be639b499ffd
Alexander Kan <alx.kan037@gmail.com>
2025-08-22 16:45:40 +0500

    Second commit

--- script_01.sh
+++ script_01.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
-
+#!/bin/env bash 
+ 
+echo Welcome!
 read -p "Please enter user name: " user
 read -p "Please enter user group: " group
 
+sleep 2
+groupadd $group
+cp /etc/sudoers{,.bkp}
+echo '%'$group' ALL=(ALL) ALL' >> /etc/sudoers
+mkdir /home/$group
+useradd $user -g $group -b /home/$group -s /bin/bash 
 
